f9bf5a419b389b0cf6125bf20dd7d3ffa4c41688
trhsy <1225408356@qq.com>
2022-09-20 20:25:54 +0800

    sj

---
+++ src/main/java/com/trhsy/sim/item/tool/ItemCopperAxe.java
@@ -0,0 +1,11 @@
+package com.trhsy.sim.item.tool;
+
+/**
+ * @author Trhsy
+ * @Package: com.trhsy.sim.item.tool
+ * @ClassName: ItemCopperAxe
+ * @Description:
+ * @date 2022/9/20 0020 下午 8:25
+ */
+public class ItemCopperAxe {
+}
