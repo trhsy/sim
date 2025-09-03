34db4afb331da23b123536bc6449db92fb89068e
trhsy <1225408356@qq.com>
2025-09-03 22:54:40 +0800

    2025年9月3日18:03:13

---
+++ src/main/java/com/trhsy/sim/ModSim.java
@@ -0,0 +1,9 @@
+package com.trhsy.sim;/**
+ * @author Trhsy
+ * @Package: com.trhsy.sim
+ * @ClassName: ModSim
+ * @Description:
+ * @date 2025/9/3 22:54
+ */
+public class ModSim {
+}
---
+++ src/main/java/com/trhsy/sim/items/item.java
@@ -0,0 +1,11 @@
+package com.trhsy.sim.items;
+
+/**
+ * @author Trhsy
+ * @Package: com.trhsy.sim.items
+ * @ClassName: item
+ * @Description:
+ * @date 2025/9/3 22:54
+ */
+public class item {
+}
